08c18a7b1f11d65db8e1fb809ce65bcd896bb838
gshah2020 <gaurang.r.shah@gmail.com>
2022-10-16 02:35:06 -0400

    add argon2 for jwt handling

--- src/lib/argon2/index.ts
+++ src/lib/argon2/index.ts
@@ -1,16 +1,12 @@
-import { noop } from '../../utils/fns';
+import argon2 from 'argon2';
 
-// import argon2 from 'argon2';
+export async function hashPassword(plainPassword: string): Promise<string> {
+  return argon2.hash(plainPassword);
+}
 
-// export async function hashPassword(plainPassword: string): Promise<string> {
-//   return argon2.hash(plainPassword);
-// }
-
-// export async function comparePasswords(
-//   plainPassword: string,
-//   hashPassword: string
-// ): Promise<boolean> {
-//   return argon2.verify(hashPassword, plainPassword);
-// }
-
-export const boop = () => noop();
+export async function comparePasswords(
+  plainPassword: string,
+  hashPassword: string
+): Promise<boolean> {
+  return argon2.verify(hashPassword, plainPassword);
+}
