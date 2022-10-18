73d924f14aa47dd17d5db85cfc9d6f4a7c8a8b60
gshah2020 <gaurang.r.shah@gmail.com>
2022-10-18 15:09:39 -0400

    add form data helper fn

--- src/utils/form/utils.ts
+++ src/utils/form/utils.ts
@@ -1,4 +1,5 @@
 export function parseFormData(input: HTMLInputElement) {
+  // form input mapping helper component used to return an array of objects for each input value.
   return {
     [input?.name]: input?.value,
   };
