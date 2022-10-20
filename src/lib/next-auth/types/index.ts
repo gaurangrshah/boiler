4604506b80dc71599ebd7ba1ac03fbecd9f2bc89
gshah2020 <gaurang.r.shah@gmail.com>
2022-10-20 17:10:19 -0400

    wip:initial spotify api

--- src/lib/next-auth/types/index.ts
+++ src/lib/next-auth/types/index.ts
@@ -1,6 +1,6 @@
+import { Session, User } from 'next-auth';
 import { BuiltInProviderType } from 'next-auth/providers';
 import { ClientSafeProvider, LiteralUnion } from 'next-auth/react';
-import { Session } from 'next-auth';
 
 export type NextAuthProvider = Record<
   LiteralUnion<BuiltInProviderType, string>,
@@ -15,4 +15,21 @@ export interface SessionWithUser extends Session {
   email: string;
   image: string;
   emailVerified?: string;
+}
+
+// Extend the build-in session types
+declare module 'next-auth' {
+  interface Session {
+    accessToken: string;
+  }
+}
+// Extend the build-in types for JWT
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken: string;
+    refreshToken: string;
+    accessTokenExpires: number;
+    user: User;
+    error?: 'RefreshAccessTokenDenied';
+  }
 }
