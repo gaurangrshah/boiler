4a1253e18a690367be4295f55793f1ae17cd5903
gshah2020 <gaurang.r.shah@gmail.com>
2022-10-25 21:49:49 -0400

    cleanup: minor

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { dev } from './utils';
 
+
 export async function middleware(req: NextRequest) {
   //token will exist if user is logged in
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
@@ -11,16 +12,17 @@ export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   if (pathname.includes('api/auth') || token) {
-    dev.log('respons.next at middleware');
+    dev.log('response.next at middleware');
     return NextResponse.next();
   }
 
   //redirect them to login if they don't have token and are
   //requesting a protected route
-  if (!token && pathname !== '/auth/signin') {
+  if (!token && pathname !== 'auth/signin') {
     console.log('redirect to login middleware');
     return NextResponse.redirect(new URL('/auth/signin', req.url));
   }
+
 }
 
 export const config = {
